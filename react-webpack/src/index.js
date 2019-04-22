6b0260100a1313d8ad990c86b48bb45004e7e782
eden <18166044122@163.com>
2019-04-22 10:32:15 +0800

    react

---
+++ react-webpack/src/index.js
@@ -0,0 +1,6 @@
+//假设它是main.js 还是我们的入口文件
+// console.log("webpack-dev-server!!!!!!!!!!")
+import React from 'react' //创建组件、虚拟dom元素 生命周期
+import ReactDOM from 'react-dom' //把创建好的组件和虚拟Dom放到页面上进行展示的
+
+//创建虚拟dom
---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,25 @@
+//向外暴露一个打包的配置对象 webpack是基于node构建的 所以用的是module.exports 所以webpack支持所有node API和语法
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: path.join(__dirname,'./src/index.html'),
+    filename: 'index.html'
+})
+
+module.exports = {
+    mode:'production',//development 生产环境（调试环境） production 产品环境 此选项为必选项
+    //在webpack 4.x，默认的打包入口文件是src->index.js 3.x需要自己配置同时一般为main.js ,4.x默认了打包的入口和输出文件 
+    plugins:[
+        htmlPlugin
+    ]
+}
+
+//webpack-dev-server生成的js文件是放在内存中的，并没有放在物理磁盘上 但是我们可以理解为隐形的main.js
+
+
+
+//目前不行 es6的api 不是node的api
+// export default{
+
+// }
