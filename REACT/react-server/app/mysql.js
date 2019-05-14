7a4597f2a87d01b5ab10f70dbf483fe7787c8d69
eden <18166044122@163.com>
2019-05-14 14:36:34 +0800

    'server'

---
+++ REACT/react-server/app/mysql.js
@@ -0,0 +1,35 @@
+const mysql = require("mysql")
+const config = require("../config/config")
+
+var pool  = mysql.createPool({
+    host     : config.database.HOST,
+    user     : config.database.USERNAME,
+    password : config.database.PASSWORD,
+    database : config.database.DATABASE,
+    port:3306
+});
+
+let query = (sql, values) => {
+    
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err)
+            } else {
+                connection.query(sql, values, (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        console.log(rows)
+                        resolve(rows)
+                    }
+                    connection.end()
+                })
+            }
+        })
+    })
+};
+
+module.exports = {
+    query
+}
---
+++ REACT/react-server/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    database:{
+        DATABASE:"blog",
+        USERNAME:"root",
+        PASSWORD:"142536",
+        PORT:3306,
+        HOST:"119.29.183.36"
+    }
+}
+module.exports = config
