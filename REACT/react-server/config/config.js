ff7d6f95a08846f99e03f4853baa72458e62431c
Eden <18166044122@163.com>
2019-06-01 20:18:09 +0800

    Update config.js

--- REACT/react-server/config/config.js
+++ REACT/react-server/config/config.js
@@ -2,7 +2,7 @@ const config = {
     database:{
         DATABASE:"blog",
         USERNAME:"root",
-        PASSWORD:"142536",
+        PASSWORD:"nmsl",
         PORT:3306,
         HOST:"119.29.183.36"
     }
